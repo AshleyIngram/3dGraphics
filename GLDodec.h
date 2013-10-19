9392e2649a426e115503d7ae42ab3ba978084f17
Ashley Ingram <ashley2504@hotmail.com>
2013-10-20 00:36:28 +0100

    Dodecohedron without lines

---
+++ GLDodec.h
@@ -0,0 +1,11 @@
+#ifndef _GL_DODEC_POLYGON_H
+#define _GL_DODEC_POLYGON_H 1
+
+#include "GLPolygon.h"
+
+class GLDodec : public GLPolygon
+{
+    public:               
+        GLDodec();
+};
+#endif
